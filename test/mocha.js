a4d2b3db52ad6d22c38168d2d0fe83df09cb49e0
Joshua T Kalis <kalisjoshua@gmail.com>
2013-01-21 18:06:04 -0500

    rewrite looking good so far

---
+++ test/mocha.js
@@ -0,0 +1,101 @@
+/*jshint*/
+/*global describe it require*/
+
+var assert          = require("chai").assert,
+    CORELogger      = require("corelogger"),
+    express         = require("express"),
+    Handlebars      = require("hbs"),
+    http            = require("http"),
+    path            = require("path"),
+
+    app             = express(),
+    Grimm           = require("../rewrite.js");
+
+describe("Grimm Framework", function () {
+  var validConfig = {
+    app: app,
+    engine: express,
+    env: require("../../../config/dev.json"),
+    root: __dirname + "/../../..",
+    server: http.createServer(app),
+    socketio: require("socket.io"),
+    templating: Handlebars
+  };
+
+  var grimm = new Grimm(validConfig);
+
+  describe("Instantiation", function () {
+    it("should be a function", function () {
+      assert.isFunction(Grimm, "Grimm is available");
+    });
+
+    it("should throw errors with no config object passed in", function () {
+      assert.throws(function () {
+        var test = new Grimm();
+        test.log();
+      }, "Required configuration object not provided to Grimm constructor.");
+    });
+
+    it("should throw errors with an empty config object", function () {
+      assert.throws(function () {
+        var test = new Grimm({});
+        test.log();
+      }, "Required configuration property [app] not provided to Grimm constructor.");
+    });
+
+    it("should throw errors when config.app is not 'express'-like", function () {
+      assert.throws(function () {
+        var test = new Grimm({
+              app: null,
+              engine: express,
+              env: true,
+              root: __dirname,
+              server: http.createServer(app),
+              socketio: require("socket.io"),
+              templating: Handlebars
+            });
+        test.log();
+      }, "Required configuration property [app] not provided to Grimm constructor.");
+
+      assert.throws(function () {
+        var test = new Grimm({
+              app: {put: []},
+              engine: express,
+              env: true,
+              root: __dirname,
+              server: http.createServer(app),
+              socketio: require("socket.io"),
+              templating: Handlebars
+            });
+        test.log();
+      }, "Required method/property not available on config.app: delete");
+    });
+
+    it("should throw errors when given an invalid logger", function () {
+      assert.throws(function () {
+        var test = new Grimm({
+              app: app,
+              engine: express,
+              env: true,
+              logger: true,
+              root: __dirname,
+              server: http.createServer(app),
+              socketio: require("socket.io"),
+              templating: Handlebars
+            });
+        test.log();
+      }, "Logger provided but is not a function.");
+    });
+
+    it("should confirm configuration properties", function () {
+      assert.equal(path.resolve(__dirname + "/../../.."), grimm.root, "grimm.root should return the path passed into the constructor, run through path.resolve.");
+
+      grimm.setLogFn(function (id) {return id;});
+      assert.equal(grimm.log("hello"), "hello", "Setting a log function works.");
+
+      assert.equal("dev", grimm.env, "grimm.env should mirror the value set in the /conf/<env>.json file");
+    });
+  });
+
+  grimm.start();
+});
